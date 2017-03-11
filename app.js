d3ae8ee3947882d7d55149caaf10702cb74967b9
Chiefautoparts <maxwelljohnson@comcast.net>
2017-03-10 18:20:39 -0800

    finally got problem 3 to work. alot of changes were made to everything.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function sum(a,b){ //eslint-disable-li
 }
 
 // Here is the test for sum(); uncomment it to run it
-sum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -25,11 +25,11 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
   var probTwo = a * b;
-  return [probTwo, 'The product of 5 and 9 is 45'];
+  return [probTwo, 'The product of 5 and 9 is 45.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-multiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -43,13 +43,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var oneTwo = sum(a,b);
-  var twoThree = sum(oneTwo[0],c);
-  return [twoThree, '4 and 7 and 5 sum to 16', 'the product of 4 and 7 and 5 is 140', oneTwo];
+  var sumTwo = sum(a,b);
+  var sumThree = sum(sumTwo[0],c);
+  var multTwo = multiply(a,b);
+  var multThree = multiply(multTwo[0],c);
+  return [sumThree[0], multThree[0], a + ' and ' + b + ' and ' + c + ' sum to 16.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is 140.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-sumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -69,7 +71,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// sumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -86,6 +88,6 @@ Test this function by hand in the console to get it working, and when you think 
 //}
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// multiplyArray(2,3,4);
+// testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
