aef2afd1251920912c4c1ff5933aa4959a31b8f0
Chiefautoparts <maxwelljohnson@comcast.net>
2017-03-11 09:26:33 -0800

    second attempt , changed string in array

--- app.js
+++ app.js
@@ -68,12 +68,12 @@ var testArray = [2,3,4]; //eslint-disable-line
 function sumArray(testArray){ //eslint-disable-line
   var sumFour = sum(testArray[0],testArray[1]);
   var sumFive = sum(sumFour[0],testArray[2]);
-  return [testArray[0], testArray[1], testArray[2] + ' was passed in as an array of numbers, and ', + sumFive + ' is there sum.'];
+  return [sumFive, '2,3,4 was passed in as an array of numbers, and 9 is their sum.'];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
